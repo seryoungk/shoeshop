b5245bbc1e2d779f6c55d7f879094df02dbe6b2d
곽세령 <dmckuromi@gmail.com>
2023-09-11 20:05:58 +0900

    ㅎㅎㅋㅋ

---
+++ src/pages/Map.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Map() {
+  return (
+    <div>
+      <Navbar />
+      지도다냥~
+    </div>
+  );
+}
+
+export default Map;
