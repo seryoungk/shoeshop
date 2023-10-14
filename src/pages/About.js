d518f764150937ae717af39cc3fb604cee319f65
곽세령 <dmckuromi@gmail.com>
2023-10-15 05:18:13 +0900

    dd

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 function About() {
+  const getItem = async () => {
+    const response = await axios.get("http://localhost:4000/items");
+    console.log(response.data);
+    return response.data;
+  };
+
   return (
     <div>
       <Navbar />
-      어바웃 페이지 입니당~
+      <h1>About Page</h1>
     </div>
   );
 }
 
 export default About;
+
+// import React, { useEffect, useState } from "react";
+// import Navbar from "../components/Navbar";
+// import axios from "axios";
+
+// function About() {
+//   const apiUrl = "http://localhost:4000";
+//   const [items, setItems] = useState([]);
+
+//   const fetchItems = async () => {
+//     try {
+//       const response = await axios.get(`${apiUrl}/items`);
+//       setItems(response.data.items);
+//       console.log(response.data.items);
+//     } catch (error) {
+//       console.error("에러났음", error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchItems();
+//   }, []);
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <h1>About Page</h1>
+//       <ul>
+//         {items &&
+//           items.map((item) => (
+//             <li key={item.id}>
+//               ID: {item.id}, Name: {item.name}
+//             </li>
+//           ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default About;
